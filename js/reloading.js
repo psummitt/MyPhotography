5e9f5de8f2e193914de401ca0e6016245b218d65
Paul M. Summitt <psummitt@gmail.com>
2026-05-19 12:24:13 -0500

    update

--- js/reloading.js
+++
@@ -1,8 +0,0 @@
-var myVar;
-function timeout() {
-  myVar = setTimeout(showPage, 3000);
-}
-function showPage() {
-  document.getElementById("loading").style.display = "none";
-  document.getElementById("homepage").style.display = "block";
-}
--- js/wheelcontroll.js
+++
@@ -1,9 +0,0 @@
-
-function toggleFunction(){
-	var x = document.getElementsById("topicbar");
-	if (x.className === "topbar") {
-		x.className += " responsive";
-	  } else {
-		x.className = "topbar";
-	  }
-}
